f21445bb2e600ff7fd1802a8b7f0d425413ac168
Emily Quevedo <37810404+EmilyQue@users.noreply.github.com>
2019-05-14 19:42:22 -0600

    Add files via upload

---
+++ Activity 5/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Activity4.Models;
+
+namespace Activity4.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<UserModel> user = new List<UserModel>();
+            user.Add(new UserModel("Emily Quevedo", "emily@email.com", "1112223333"));
+            user.Add(new UserModel("Mickey Mouse", "mickey@mouse.com", "9998887777"));
+            user.Add(new UserModel("Donald Duck", "donald@duck.com", "4445556666"));
+
+            return View("Test", user);
+        }
+    }
+}
